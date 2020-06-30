3e25c1d85e48d2b3f7d3478d6c5c5c831ce2be60
JOEL <joeltovar2047@gmail.com>
2020-06-30 09:09:07 -0600

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,15 +8,28 @@ import { InfoPagina } from '../interfaces/info-pagina-interface';
 export class InfoPaginaService {
     info: InfoPagina = {};
     cargada: boolean = false;
+    equipo: any[] = [];
 
     constructor( private http: HttpClient) {
-    // Leer arvhivo JSON
-    this.http.get('assets/data/data-pagina.json') 
+        this.cargarInfo();
+        this.cargarEquipo();
+    }
+
+    private cargarInfo() {
+        // Leer arvhivo JSON
+        this.http.get('assets/data/data-pagina.json') 
         .subscribe( (resp: InfoPagina) => {
             this.cargada = true;
             this.info    = resp;
-            console.log(resp);
-            
         })
+    }
+
+    private cargarEquipo() {
+        
+        this.http.get('https://angular-html-3e91d.firebaseio.com/equipo.json') 
+        .subscribe( (resp : any[]) => {
+            this.equipo    = resp;
+        })
+
     }
 }
